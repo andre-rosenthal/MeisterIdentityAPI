1165f84f9fed75fd2cddb059393a85ab0e8488cb
Andre Rosenthal <andre@s4meister.com>
2021-08-08 05:03:48 -0500

    API Baseline done

--- MeiserIdentityAPI/Startup.cs
+++ MeiserIdentityAPI/Startup.cs
@@ -26,7 +26,7 @@ namespace MeiserIdentityAPI
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
