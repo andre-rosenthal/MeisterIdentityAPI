941af390d78b1106b6f5940910009e50844e0cd5
Andre Rosenthal <andre@s4meister.com>
2021-08-08 04:24:53 -0500

    Add project files.

---
+++ MeiserIdentityAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MeiserIdentityAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
